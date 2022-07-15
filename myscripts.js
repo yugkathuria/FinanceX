438671fb5aa73061a4fa8550ccd014480bc273cb
Yug <yugkathuria@gmail.com>
2022-07-15 13:15:44 +0530

    first

---
+++ myscripts.js
@@ -0,0 +1,259 @@
+    function cal1(){
+        var n0 = document.getElementById("n0").value;
+        document.getElementById("name1").innerHTML = n0;
+
+        var n01 = document.getElementById("n01").value;
+        document.getElementById("fy1").innerHTML = n01;
+        
+
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = document.getElementById("n3").value;
+    var n4 = document.getElementById("n4").value;
+    var n5 = document.getElementById("n5").value;
+    var n6 = document.getElementById("n6").value;
+    var n7 = document.getElementById("n7").value;
+    var n8 = document.getElementById("n8").value;
+    var n9 = document.getElementById("n9").value;
+    var n10 = document.getElementById("n10").value;
+    var n11 = document.getElementById("n11").value;
+    var n12 = document.getElementById("n12").value;
+    var n13 = document.getElementById("n13").value;
+    var n14 = document.getElementById("n14").value;
+    var n15 = document.getElementById("n15").value;
+    var n16 = document.getElementById("n16").value;
+    var n17 = document.getElementById("n17").value;
+    var n18 = document.getElementById("n18").value;
+    var n19 = document.getElementById("n19").value;
+    var n20 = document.getElementById("n20").value;
+    var n21 = document.getElementById("n21").value;
+    var n22 = document.getElementById("n22").value;
+    var n23 = document.getElementById("n23").value;
+    var n24 = document.getElementById("n24").value;
+    var n25 = document.getElementById("n25").value;
+    var n26 = document.getElementById("n26").value;
+    var n27 = document.getElementById("n27").value;
+    var n28 = document.getElementById("n28").value;
+    var n29 = document.getElementById("n29").value;
+    var n30 = document.getElementById("n30").value;
+    var n31 = document.getElementById("n31").value;
+    var n32 = document.getElementById("n32").value;
+
+    
+    document.getElementById("r1").innerHTML = n1/n2;
+    document.getElementById("r2").innerHTML = (n1-n3)/n2;
+    document.getElementById("r3").innerHTML = (n4+n5)/n2;
+    document.getElementById("r4").innerHTML = n6/(n2/365);
+    document.getElementById("r5").innerHTML = n8/n9;
+    document.getElementById("r6").innerHTML = n10+n11-n12;
+    document.getElementById("r7").innerHTML = n13/n14;
+    document.getElementById("r8").innerHTML = n13/n15;
+    document.getElementById("r9").innerHTML = n16/n17;
+    document.getElementById("r10").innerHTML = n18/n19;
+    document.getElementById("r11").innerHTML = n20/n21;
+    document.getElementById("r12").innerHTML = (n13-n8)/n13;
+    document.getElementById("r13").innerHTML = n20/n13;
+    document.getElementById("r14").innerHTML = (n22)/n13;
+    document.getElementById("r15").innerHTML = (n22+n23)/n15;
+    document.getElementById("r16").innerHTML = n22/n24;
+    document.getElementById("r17").innerHTML = n25/n26;
+    document.getElementById("r18").innerHTML = n25/n28;
+    document.getElementById("r19").innerHTML = n25/n30;
+    document.getElementById("r20").innerHTML = n32/n25;
+    }
+    function cal2(){
+        var m0 = document.getElementById("m0").value;
+        document.getElementById("name2").innerHTML = m0;
+
+        var m01 = document.getElementById("m01").value;
+        document.getElementById("fy2").innerHTML = m01;
+
+    var m1 = document.getElementById("m1").value;
+    var m2 = document.getElementById("m2").value;
+    var m3 = document.getElementById("m3").value;
+    var m4 = document.getElementById("m4").value;
+    var m5 = document.getElementById("m5").value;
+    var m6 = document.getElementById("m6").value;
+    var m7 = document.getElementById("m7").value;
+    var m8 = document.getElementById("m8").value;
+    var m9 = document.getElementById("m9").value;
+    var m10 = document.getElementById("m10").value;
+    var m11 = document.getElementById("m11").value;
+    var m12 = document.getElementById("m12").value;
+    var m13 = document.getElementById("m13").value;
+    var m14 = document.getElementById("m14").value;
+    var m15 = document.getElementById("m15").value;
+    var m16 = document.getElementById("m16").value;
+    var m17 = document.getElementById("m17").value;
+    var m18 = document.getElementById("m18").value;
+    var m19 = document.getElementById("m19").value;
+    var m20 = document.getElementById("m20").value;
+    var m21 = document.getElementById("m21").value;
+    var m22 = document.getElementById("m22").value;
+    var m23 = document.getElementById("m23").value;
+    var m24 = document.getElementById("m24").value;
+    var m25 = document.getElementById("m25").value;
+    var m26 = document.getElementById("m26").value;
+    var m27 = document.getElementById("m27").value;
+    var m28 = document.getElementById("m28").value;
+    var m29 = document.getElementById("m29").value;
+    var m30 = document.getElementById("m30").value;
+    var m31 = document.getElementById("m31").value;
+    var m32 = document.getElementById("m32").value;
+    
+    
+   
+     
+    document.getElementById("r21").innerHTML = m1/m2;
+    document.getElementById("r22").innerHTML = (m1-m3)/m2;
+    document.getElementById("r23").innerHTML = (m4+m5)/m2;
+    document.getElementById("r24").innerHTML = m6/(m2/365);
+    document.getElementById("r25").innerHTML = m8/m9;
+    document.getElementById("r26").innerHTML = m10+m11-m12;
+    document.getElementById("r27").innerHTML = m13/m14;
+    document.getElementById("r28").innerHTML = m13/m15;
+    document.getElementById("r29").innerHTML = m16/m17;
+    document.getElementById("r30").innerHTML = m18/m19;
+    document.getElementById("r31").innerHTML = m20/m21;
+    document.getElementById("r32").innerHTML = (m13-m8)/m13;
+    document.getElementById("r33").innerHTML = m20/m13;
+    document.getElementById("r34").innerHTML = (m22)/m13;
+    document.getElementById("r35").innerHTML = (m22+m23)/m15;
+    document.getElementById("r36").innerHTML = m22/m24;
+    document.getElementById("r37").innerHTML = m25/m26;
+    document.getElementById("r38").innerHTML = m25/m28;
+    document.getElementById("r39").innerHTML = m25/m30;
+    document.getElementById("r40").innerHTML = m32/m25;
+    }
+    function cal3(){
+        var p0 = document.getElementById("p0").value;
+        document.getElementById("name3").innerHTML = p0;
+
+        var p01 = document.getElementById("p01").value;
+        document.getElementById("fy3").innerHTML = p01;
+
+        var p1 = document.getElementById("p1").value;
+        var p2 = document.getElementById("p2").value;
+        var p3 = document.getElementById("p3").value;
+        var p4 = document.getElementById("p4").value;
+        var p5 = document.getElementById("p5").value;
+        var p6 = document.getElementById("p6").value;
+        var p7 = document.getElementById("p7").value;
+        var p8 = document.getElementById("p8").value;
+        var p9 = document.getElementById("p9").value;
+        var p10 = document.getElementById("p10").value;
+        var p11 = document.getElementById("p11").value;
+        var p12 = document.getElementById("p12").value;
+        var p13 = document.getElementById("p13").value;
+        var p14 = document.getElementById("p14").value;
+        var p15 = document.getElementById("p15").value;
+        var p16 = document.getElementById("p16").value;
+        var p17 = document.getElementById("p17").value;
+        var p18 = document.getElementById("p18").value;
+        var p19 = document.getElementById("p19").value;
+        var p20 = document.getElementById("p20").value;
+        var p21 = document.getElementById("p21").value;
+        var p22 = document.getElementById("p22").value;
+        var p23 = document.getElementById("p23").value;
+        var p24 = document.getElementById("p24").value;
+        var p25 = document.getElementById("p25").value;
+        var p26 = document.getElementById("p26").value;
+        var p27 = document.getElementById("p27").value;
+        var p28 = document.getElementById("p28").value;
+        var p29 = document.getElementById("p29").value;
+        var p30 = document.getElementById("p30").value;
+        var p31 = document.getElementById("p31").value;
+        var p32 = document.getElementById("p32").value;
+        
+        
+       
+         
+        document.getElementById("r41").innerHTML = p1/p2;
+        document.getElementById("r42").innerHTML = (p1-p3)/p2;
+        document.getElementById("r43").innerHTML = (p4+p5)/p2;
+        document.getElementById("r44").innerHTML = p6/(p2/365);
+        document.getElementById("r45").innerHTML = p8/p9;
+        document.getElementById("r46").innerHTML = p10+p11-p12;
+        document.getElementById("r47").innerHTML = p13/p14;
+        document.getElementById("r48").innerHTML = p13/p15;
+        document.getElementById("r49").innerHTML = p16/p17;
+        document.getElementById("r50").innerHTML = p18/p19;
+        document.getElementById("r51").innerHTML = p20/p21;
+        document.getElementById("r52").innerHTML = (p13-p8)/p13;
+        document.getElementById("r53").innerHTML = p20/p13;
+        document.getElementById("r54").innerHTML = (p22)/p13;
+        document.getElementById("r55").innerHTML = (p22+p23)/p15;
+        document.getElementById("r56").innerHTML = p22/p24;
+        document.getElementById("r57").innerHTML = p25/p26;
+        document.getElementById("r58").innerHTML = p25/p28;
+        document.getElementById("r59").innerHTML = p25/p30;
+        document.getElementById("r60").innerHTML = p32/p25;
+        }
+        function cal4(){
+            var q0 = document.getElementById("q0").value;
+            document.getElementById("name4").innerHTML = q0;
+
+            var q01 = document.getElementById("q01").value;
+            document.getElementById("fy4").innerHTML = q01;
+
+            var q1 = document.getElementById("q1").value;
+            var q2 = document.getElementById("q2").value;
+            var q3 = document.getElementById("q3").value;
+            var q4 = document.getElementById("q4").value;
+            var q5 = document.getElementById("q5").value;
+            var q6 = document.getElementById("q6").value;
+            var q7 = document.getElementById("q7").value;
+            var q8 = document.getElementById("q8").value;
+            var q9 = document.getElementById("q9").value;
+            var q10 = document.getElementById("q10").value;
+            var q11 = document.getElementById("q11").value;
+            var q12 = document.getElementById("q12").value;
+            var q13 = document.getElementById("q13").value;
+            var q14 = document.getElementById("q14").value;
+            var q15 = document.getElementById("q15").value;
+            var q16 = document.getElementById("q16").value;
+            var q17 = document.getElementById("q17").value;
+            var q18 = document.getElementById("q18").value;
+            var q19 = document.getElementById("q19").value;
+            var q20 = document.getElementById("q20").value;
+            var q21 = document.getElementById("q21").value;
+            var q22 = document.getElementById("q22").value;
+            var q23 = document.getElementById("q23").value;
+            var q24 = document.getElementById("q24").value;
+            var q25 = document.getElementById("q25").value;
+            var q26 = document.getElementById("q26").value;
+            var q27 = document.getElementById("q27").value;
+            var q28 = document.getElementById("q28").value;
+            var q29 = document.getElementById("q29").value;
+            var q30 = document.getElementById("q30").value;
+            var q31 = document.getElementById("q31").value;
+            var q32 = document.getElementById("q32").value;
+            
+            
+           
+             
+            document.getElementById("r61").innerHTML = q1/q2;
+            document.getElementById("r62").innerHTML = (q1-q3)/q2;
+            document.getElementById("r63").innerHTML = (q4+q5)/q2;
+            document.getElementById("r64").innerHTML = q6/(q2/365);
+            document.getElementById("r65").innerHTML = q8/q9;
+            document.getElementById("r66").innerHTML = q10+q11-q12;
+            document.getElementById("r67").innerHTML = q13/q14;
+            document.getElementById("r68").innerHTML = q13/q15;
+            document.getElementById("r69").innerHTML = q16/q17;
+            document.getElementById("r70").innerHTML = q18/q19;
+            document.getElementById("r71").innerHTML = q20/q21;
+            document.getElementById("r72").innerHTML = (q13-q8)/q13;
+            document.getElementById("r73").innerHTML = q20/q13;
+            document.getElementById("r74").innerHTML = (q22)/q13;
+            document.getElementById("r75").innerHTML = (q22+q23)/q15;
+            document.getElementById("r76").innerHTML = q22/q24;
+            document.getElementById("r77").innerHTML = q25/q26;
+            document.getElementById("r78").innerHTML = q25/q28;
+            document.getElementById("r79").innerHTML = q25/q30;
+            document.getElementById("r80").innerHTML = q32/q25;
+            }
+            function printcard(){
+                window.print();
+                }
+    
